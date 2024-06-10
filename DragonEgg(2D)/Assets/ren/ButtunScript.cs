2e359b982f2fe40057009a7f15ced754eb7b6eaf
lovecap7 <166367138+lovecap7@users.noreply.github.com>
2024-06-10 17:34:26 +0900

    20240610_ren

--- DragonEgg(2D)/Assets/ren/ButtunScript.cs
+++ DragonEgg(2D)/Assets/ren/ButtunScript.cs
@@ -4,24 +4,47 @@ using UnityEngine;
 
 public class ButtunScript : MonoBehaviour
 {
-    public GameObject camera;
+    //private Vector2 tmp;
+    //public GameObject camera;
+    //public static float x, y;
 
-    // ボタンが押された場合、今回呼び出される関数
-   
-    // Start is called before the first frame update
-    void Start()
-    {
-        camera = GameObject.Find("CameraController");
-    }
+    //// ボタンが押された場合、今回呼び出される関数
 
-    // Update is called once per frame
-    void Update()
-    {
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    camera = GameObject.Find("CameraController");
 
-    }
-    public void OnClick()
-    {
-        Debug.Log("押された!");  // ログを出力
-        BootsupandsquadManager.CameraMove1();
-    }
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    tmp = GameObject.Find("Main Camera").transform.position;
+    //    x = tmp.x;
+    //    y = tmp.y;
+    //    if (Input.GetKeyDown(KeyCode.Escape))//強化画面からモンスターBOXへ
+    //    {
+    //        CameraContoller.CameraMove1();
+
+    //    }
+
+    //    if (CameraContoller.flag == 1)
+    //    {
+    //        CameraContoller.powerUpCanvas.SetActive(false);
+    //        CameraContoller.canvas.SetActive(true);
+    //        CameraContoller.MonsterBox(y);
+    //    }
+    //    else if (CameraContoller.flag == 0)
+    //    {
+    //        CameraContoller.powerUpCanvas.SetActive(true);
+    //        CameraContoller.canvas.SetActive(false);
+    //    }
+    //}
+    //public void OnClick()
+    //{
+    //    Debug.Log("押された!");  // ログを出力
+    //    CameraContoller.CameraMove1();
+    //}
+
 }
--- DragonEgg(2D)/Assets/ren/CameraContoller.cs
+++ DragonEgg(2D)/Assets/ren/CameraContoller.cs
@@ -1,27 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static Unity.Burst.Intrinsics.X86.Avx;
+using UnityEngine.UI;
 
-public class BootsupandsquadManager : MonoBehaviour
+public class CameraContoller : MonoBehaviour
 {
     private Vector2 tmp;
-    GameObject powerUpCanvas;
+    public static GameObject powerUpCanvas;
+    //public static GameObject canvas;//あとで変更
     public static int flag = 1;
+    public GameObject camera;
+    float x, y;
 
     // Start is called before the first frame update
     void Start()
     {
-       // tmp = GameObject.Find("Main Camera").transform.position;
+        // tmp = GameObject.Find("Main Camera").transform.position;
         powerUpCanvas = GameObject.Find("PowerUpCanvas");
+        //canvas = GameObject.Find("Canvas");
+        //camera = GameObject.Find("CameraController");
     }
 
     // Update is called once per frame
     void Update()
     {
         tmp = GameObject.Find("Main Camera").transform.position;
-        float x = tmp.x;
-        float y = tmp.y;
+        x = tmp.x;
+        y = tmp.y;
         //Debug.Log(y);
 
         if (Input.GetKeyDown(KeyCode.Escape))//強化画面からモンスターBOXへ
@@ -38,18 +43,20 @@ public class BootsupandsquadManager : MonoBehaviour
         if (flag == 1)
         {
             powerUpCanvas.SetActive(false);
+            //canvas.SetActive(true);
             MonsterBox(y);
         }
         else if (flag == 0)
         {
             powerUpCanvas.SetActive(true);
+            //canvas.SetActive(false);
         }
 
     }
 
     public static void CameraMove1()
     {
-        GameObject.Find("Main Camera").transform.position = new Vector3(0, 0, -10);
+        GameObject.Find("Main Camera").transform.position = new Vector3(-80, 0, -10);
         flag = 1;
     }
     public static void CameraMove2()
@@ -58,7 +65,7 @@ public class BootsupandsquadManager : MonoBehaviour
         flag = 0;
     }
 
-    void MonsterBox(float y)//カメラがきれいにy=0で止まらない　動作的には問題ないけど気になるから今後修正
+    public static void MonsterBox(float y)//カメラがきれいにy=0で止まらない　動作的には問題ないけど気になるから今後修正
     {
         if (Input.GetKey(KeyCode.UpArrow) && flag != 0 && y <= 0.00f)
         {
@@ -69,5 +76,11 @@ public class BootsupandsquadManager : MonoBehaviour
             GameObject.Find("Main Camera").transform.position += new Vector3(0.0f, -0.01f, 0.0f); //カメラを下へ移動。
         }
     }
-}
 
+
+    //public void OnClick()
+    //{
+    //    Debug.Log("押された!");  // ログを出力
+    //    CameraMove1();
+    //}
+}
