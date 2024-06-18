2bb4c43527bacd6d3459f6fc5146dfb744cfc1f7
meronpanunn <166367524+meronpanunn@users.noreply.github.com>
2024-06-18 17:32:48 +0900

    yurube_20240618

--- DragonEgg(2D)/Assets/eito/test.cs
+++ DragonEgg(2D)/Assets/eito/test.cs
@@ -5,21 +5,21 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;  // UIコンポーネントの使用
 public class test : MonoBehaviour
 {
-    [SerializeField] Button focusButton;
-    // Start is called before the first frame update
-    void Start()
-    {
-        // ボタンコンポーネントの取得
-        focusButton = focusButton.GetComponent<Button>();
-    }
+    //[SerializeField] Button focusButton;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    // ボタンコンポーネントの取得
+    //    focusButton = focusButton.GetComponent<Button>();
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        //全てのフォーカスを解除する
-        EventSystem.current.SetSelectedGameObject(null);
-        //focusButtonにフォーカスする
-        focusButton.Select();
-        //Canvasコンポーネントを無効にする。Buttonコンポーネントで設定可。
-    }
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    //全てのフォーカスを解除する
+    //    EventSystem.current.SetSelectedGameObject(null);
+    //    //focusButtonにフォーカスする
+    //    focusButton.Select();
+    //    //Canvasコンポーネントを無効にする。Buttonコンポーネントで設定可。
+    //}
 }
