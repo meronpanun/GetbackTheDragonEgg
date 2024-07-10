88e62b93a837bb51ae8a0c8ba6999e1ffcf9647a
YukiCreative <166368392+YukiCreative@users.noreply.github.com>
2024-07-10 13:20:06 +0900

    Yuki_20240710_13:20

--- DragonEgg(2D)/Assets/nakahira/ChildDragon/ChildDragonController.cs
+++ DragonEgg(2D)/Assets/nakahira/ChildDragon/ChildDragonController.cs
@@ -1,22 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using Unity.VisualScripting;
+//using UnityEngine;
 
-public class ChildDragonController : MonoBehaviour
-{
-    [SerializeField]
-    private static Sprite fire;
-    [SerializeField]
-    private static Sprite ice;
-    [SerializeField]
-    private static Sprite wind;
-    [SerializeField]
-    private static Sprite thunder;
+//public class ChildDragonController : MonoBehaviour
+//{
+//    [SerializeField]
+//    private Animation parent;
+//    [SerializeField]
+//    private Animation fire;
+//    [SerializeField]
+//    private Animation ice;
+//    [SerializeField]
+//    private Animation wind;
+//    [SerializeField]
+//    private Animation thunder;
+//    [SerializeField]
+//    private Animation empty;
 
-    private TestDragonStatus myStatus;
+//    private TestDragonStatus myStatus;
+//    private Animator myAnimator;
 
-    private void Start()
-    {
-        myStatus = BattleTeam.sChildDragonDataLeft;
-    }
-}
+//    private void Start()
+//    {
+//        myAnimator = GetComponent<Animator>();
+//        myAnimator.
+//        // ï“ê¨Ç©ÇÁèÓïÒÇéÛÇØéÊÇÈ
+//        myStatus = BattleTeam.sChildDragonDataLeft;
+
+//        switch(myStatus.raceNum)
+//        {
+//            case 0:
+//                myRenderer.sprite = parent;
+//                break;
+//            case 1:
+//                myRenderer.sprite = fire;
+//                break;
+//            case 2:
+//                myRenderer.sprite = ice;
+//                break;
+//            case 3:
+//                myRenderer.sprite = wind;
+//                break;
+//            case 4:
+//                myRenderer.sprite = thunder;
+//                break;
+//            default:
+//                myRenderer.sprite = empty;
+//                break;
+//        }
+//    }
+
+
+//}
