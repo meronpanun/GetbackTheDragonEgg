b58b289e3cb7f7961846a53802e323931e69c5c0
YAKAN3105 <shanyexiongqing@gmail.com>
2024-07-30 17:03:09 +0900

    yusei20240730

--- DragonEgg(2D)/Assets/yusei_BGM,SE/warningManager.cs
+++ DragonEgg(2D)/Assets/yusei_BGM,SE/warningManager.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class warningManager : MonoBehaviour
 {
+    Image fadeImage;
+    float time = 0.0f;
+    float fedeSpeed = 1.0f;
+    CanvasGroup Transparency;
+    bool isFadewarning = false;
+
+    Transform cam;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Transparency = GetComponent<CanvasGroup>();
+        fadeImage = GetComponent<Image>();
+        cam = GameObject.Find("Main Camera").GetComponent<Transform>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (cam.transform.position.y> 20)
+        {
+            StartCoroutine(FadeIn());
+
+            Debug.Log("Test");
+        }
+        if (cam.transform.position.y > 25)
+        {
+            StartCoroutine(FadeOut());
+
+            Debug.Log("Test2");
+        }
     }
+    IEnumerator FadeIn()
+    {
+        Transparency.alpha += 0.01f;
+        yield return new WaitForSeconds(Time.deltaTime);
+    }
+    IEnumerator FadeOut()
+    {
+        Transparency.alpha -= 0.01f;
+        yield return new WaitForSeconds(Time.deltaTime);
+    }
+
 }
