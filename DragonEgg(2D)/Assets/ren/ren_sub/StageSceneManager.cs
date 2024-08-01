127b26bcf53ddcfafd945ddf658e6eef54aa1ff4
lovecap7 <166367138+lovecap7@users.noreply.github.com>
2024-08-01 16:54:44 +0900

    20240801_ren
    コントローラーした

--- DragonEgg(2D)/Assets/ren/ren_sub/StageSceneManager.cs
+++ DragonEgg(2D)/Assets/ren/ren_sub/StageSceneManager.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StageSceneManager : MonoBehaviour
 {
     StageLoadSceneData stageLoadSceneData;
     string sceneName;
+    
     // Start is called before the first frame update
     void Start()
     {
