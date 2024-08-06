6ea73514200ce2ce049777c30868d5f7ace48c87
YAKAN3105 <shanyexiongqing@gmail.com>
2024-08-06 14:16:36 +0900

    yusei0806

--- DragonEgg(2D)/Assets/yusei_BGM,SE/warningManager.cs
+++ DragonEgg(2D)/Assets/yusei_BGM,SE/warningManager.cs
@@ -18,18 +18,31 @@ public class warningManager : MonoBehaviour
         Transparency = GetComponent<CanvasGroup>();
         fadeImage = GetComponent<Image>();
         cam = GameObject.Find("Main Camera").GetComponent<Transform>();
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (cam.transform.position.y> 20)
+        if (cam.transform.position.y> 20 && cam.transform.position.y < 21.5f)
         {
             StartCoroutine(FadeIn());
 
             Debug.Log("Test");
         }
-        if (cam.transform.position.y > 25)
+        else if (cam.transform.position.y > 21.5f && cam.transform.position.y < 23)
+        {
+            StartCoroutine(FadeOut());
+
+            Debug.Log("Test2");
+        }
+        else if (cam.transform.position.y> 23 && cam.transform.position.y < 24.5f)
+        {
+            StartCoroutine(FadeIn());
+
+            Debug.Log("Test1");
+        }
+        else if (cam.transform.position.y > 24.5f)
         {
             StartCoroutine(FadeOut());
 
@@ -38,12 +51,12 @@ public class warningManager : MonoBehaviour
     }
     IEnumerator FadeIn()
     {
-        Transparency.alpha += 0.01f;
+        Transparency.alpha += 0.02f;
         yield return new WaitForSeconds(Time.deltaTime);
     }
     IEnumerator FadeOut()
     {
-        Transparency.alpha -= 0.01f;
+        Transparency.alpha -= 0.02f;
         yield return new WaitForSeconds(Time.deltaTime);
     }
 
