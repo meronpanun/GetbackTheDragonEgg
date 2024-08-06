d4a247c4f47f67bc80f584170743b6ff8da74a2c
YukiCreative <166368392+YukiCreative@users.noreply.github.com>
2024-08-06 19:02:00 +0900

    Revert "Merge branch 'main' into YukI_20240806"
    This reverts commit 5b41cc31b575e0a21f9076e9f0fe88d2e1ba6821, reversing
    changes made to a2884a47ecaa5fc2cdf7ddce3481699ffa2b75bb.

--- DragonEgg(2D)/Assets/yusei_BGM,SE/warningManager.cs
+++ DragonEgg(2D)/Assets/yusei_BGM,SE/warningManager.cs
@@ -10,8 +10,6 @@ public class warningManager : MonoBehaviour
     float fedeSpeed = 1.0f;
     CanvasGroup Transparency;
     bool isFadewarning = false;
-    float fadeStart = 24f;
-    float fadeEnd = 0.3f;
 
     Transform cam;
     // Start is called before the first frame update
@@ -20,31 +18,18 @@ public class warningManager : MonoBehaviour
         Transparency = GetComponent<CanvasGroup>();
         fadeImage = GetComponent<Image>();
         cam = GameObject.Find("Main Camera").GetComponent<Transform>();
-
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (cam.transform.position.y> fadeStart && cam.transform.position.y < fadeStart + fadeEnd)
+        if (cam.transform.position.y> 20)
         {
             StartCoroutine(FadeIn());
 
             Debug.Log("Test");
         }
-        else if (cam.transform.position.y > fadeStart + fadeEnd && cam.transform.position.y < fadeStart + fadeEnd*2 )
-        {
-            StartCoroutine(FadeOut());
-
-            Debug.Log("Test2");
-        }
-        else if (cam.transform.position.y> fadeStart + fadeEnd *2 && cam.transform.position.y < fadeStart + fadeEnd*3)
-        {
-            StartCoroutine(FadeIn());
-
-            Debug.Log("Test1");
-        }
-        else if (cam.transform.position.y > fadeStart + fadeEnd * 3)
+        if (cam.transform.position.y > 25)
         {
             StartCoroutine(FadeOut());
 
@@ -53,12 +38,12 @@ public class warningManager : MonoBehaviour
     }
     IEnumerator FadeIn()
     {
-        Transparency.alpha += 0.03f;
+        Transparency.alpha += 0.01f;
         yield return new WaitForSeconds(Time.deltaTime);
     }
     IEnumerator FadeOut()
     {
-        Transparency.alpha -= 0.03f;
+        Transparency.alpha -= 0.01f;
         yield return new WaitForSeconds(Time.deltaTime);
     }
 
