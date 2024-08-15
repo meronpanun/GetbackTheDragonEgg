aee7cb09d208ab8b5bdcdc1af0272ff7bbe57c01
YukiCreative <166368392+YukiCreative@users.noreply.github.com>
2024-08-15 18:46:53 +0900

    Yuki_20240815_18:46
    nya

---
+++ DragonEgg(2D)/Assets/yusei_BGM,SE/ExitButtonController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButtonController : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
