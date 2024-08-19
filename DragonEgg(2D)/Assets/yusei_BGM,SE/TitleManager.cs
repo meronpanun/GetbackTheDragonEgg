6ce87994323d676fdfd1456a7ba2efb96096c809
Reo000r <166367662+Reo000r@users.noreply.github.com>
2024-08-19 20:41:16 +0900

    reo_20240819_20:41

--- DragonEgg(2D)/Assets/yusei_BGM,SE/TitleManager.cs
+++ DragonEgg(2D)/Assets/yusei_BGM,SE/TitleManager.cs
@@ -1,17 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TitleManager : MonoBehaviour
 {
-    int value;
-    public GameObject title;
-    FadeManager fadeManager;
-    public int savedate;
+    [SerializeField] private AudioSource _negativeSe;
+    [SerializeField] private AudioSource _positiveSe;
+
+    [SerializeField] private TextMeshProUGUI _dialogText;
+
+    [SerializeField] private Image _continueButtonImage;
+
+    [SerializeField] private GameObject _dialogPanel;
+    [SerializeField] private GameObject _noButton;
+    [SerializeField] private GameObject _goButton;
+
+    [SerializeField] private FadeManager fadeManager;
+
+    private const string kPlayerPrefsKey = "Progress";
+    private const float kStopColor = 0.6f;
+
+    private float red, green, blue, alpha;
+    private int value;
+    private const float kMsgSpeed = 0.09f;
+    private string dialogText = "";
+
     // Start is called before the first frame update
-     void Start()
+    void Start()
     {
+        //Debug.Log($"{kPlayerPrefsKey} Change");
+        //PlayerPrefs.SetInt(kPlayerPrefsKey, 0);
         
+        value = PlayerPrefs.GetInt(kPlayerPrefsKey);
+
+        red = _continueButtonImage.color.r;
+        green = _continueButtonImage.color.g;
+        blue = _continueButtonImage.color.b;
+        alpha = _continueButtonImage.color.a;
+
+        if (value == 0)
+        {
+            red = kStopColor;
+            green = kStopColor;
+            blue = kStopColor;
+        }
+
+        _continueButtonImage.color = new Color(red, green, blue, alpha);
     }
 
     // Update is called once per frame
@@ -20,19 +56,96 @@ public class TitleManager : MonoBehaviour
       
     }
 
-    public void OnClickContinue()
+    public void OnClickNewGame()
     {
-        value = PlayerPrefs.GetInt("Progress");
+        _positiveSe.Play();
 
-        // セーブデータがなかったら押せない
-        if (value == 0)
+        value = PlayerPrefs.GetInt(kPlayerPrefsKey);
+
+        if (value != 0)
         {
-            return;
-
+            // プレイ済の場合
+            _dialogText.text = "";
+            dialogText = "|Start New Game?";
+            StartCoroutine(ScaleChange());
         }
         else
         {
-            fadeManager.FadeOutSwitch(100);
+            fadeManager.FadeOutSwitch(101);
         }
+    }
+
+    public void OnClickContinue()
+    {
+        value = PlayerPrefs.GetInt(kPlayerPrefsKey);
+
+        // セーブデータがなかったら(0の場合)押せない
+        if (value == 0)
+        {
+            _negativeSe.Play();
+            return;
+        }
+        else
+        {
+            _positiveSe.Play();
+            fadeManager.FadeOutSwitch(101);
+        }
+    }
+
+    IEnumerator ScaleChange()
+    {
+        _noButton.SetActive(false);
+        _goButton.SetActive(false);
+        _dialogPanel.SetActive(true);
+
+        // 徐々に大きくする
+        for (int i = 0; i < 10; i++)
+        {
+            _dialogPanel.transform.localScale = new Vector3(i / 10f, i / 10f, 1);
+            yield return new WaitForSeconds(0.06f);
+        }
+        // scaleを1にする(保険)
+        _dialogPanel.transform.localScale = new Vector3(1, 1, 1);
+        yield return new WaitForSeconds(0.1f);
+
+        StartCoroutine(TypeDisplay());  // メッセージ表示を開始
+    }
+
+    IEnumerator TypeDisplay()
+    {
+        _noButton.SetActive(false);
+        _goButton.SetActive(false);
+
+        foreach (char item in dialogText)
+        {
+            if (item == '|')
+            {
+                // <br>をDialogText.textに代入する
+                _dialogText.text += "<br>";
+                yield return 0;
+            }
+            else
+            {
+                _dialogText.text += item;
+            }
+
+            // 文字をkMsgSpeed毎に表示
+            yield return new WaitForSeconds(kMsgSpeed);  
+        }
+        _noButton.SetActive(true);
+        _goButton.SetActive(true);
+    }
+
+    public void OnclickNoButton()
+    {
+        _negativeSe.Play();
+        _dialogPanel.SetActive(false);
+    }
+
+    public void OnclickGoButton()
+    {
+        PlayerPrefs.SetInt(kPlayerPrefsKey, 0);
+        _positiveSe.Play();
+        fadeManager.FadeOutSwitch(101);
     }
 }
